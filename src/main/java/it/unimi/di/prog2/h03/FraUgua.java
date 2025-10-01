fdfd5d60baac6b52731ba58f88b6f2505fcf93e2
Diego Vittorio Delfanti <diego@delfanti.com>
2025-10-01 12:05:30 +0200

    frazioni uguali con problema delle approssimazioni

---
+++ src/main/java/it/unimi/di/prog2/h03/FraUgua.java
@@ -0,0 +1,28 @@
+package it.unimi.di.prog2.h03;
+
+public class FraUgua {
+    public static void main(String[] args) {
+        // così non va bene, args è un array di stringhe e non di interi
+        /*
+         * int a = args[0];
+         * int b = args[1];
+         * int c = args[2];
+         * int d = args[3];
+         */
+
+        int a = Integer.parseInt(args[0]);
+        int b = Integer.parseInt(args[1]);
+        int c = Integer.parseInt(args[2]);
+        int d = Integer.parseInt(args[3]);
+
+        System.out.println(
+                (double) a / b == (double) c / d ? "uguali" : "diversi");
+    }
+
+    /*
+     * per eseguire il programma:
+     * ./gradlew build
+     * e poi questo con i parametri alla fine
+     * java -cp build/classes/java/main it.unimi.di.prog2.h03.FraUgua 1 2 2 4
+     */
+}
