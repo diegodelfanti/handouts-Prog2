2a3cc9fca22f95d7816cfcea20c6f0033ff3578e
Diego Vittorio Delfanti <diego@delfanti.com>
2025-10-03 11:30:37 +0200

    bandiera vettore con liste

--- src/main/java/it/unimi/di/prog2/h04/BandieraVettore_DVD.java
+++ src/main/java/it/unimi/di/prog2/h04/BandieraVettore_DVD.java
@@ -1,5 +1,7 @@
 package it.unimi.di.prog2.h04;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 import java.util.Vector;
 
@@ -7,9 +9,15 @@ import java.util.Vector;
 
 public class BandieraVettore_DVD {
     public static void main(String[] args) {
-        Vector parole = new Vector(); // vettore è un contenitore di oggetti (Object), qualsiasi. sta al programmatore
-                                      // stare attento a cosa ci mette dentro e a sapere cosa tira fuori quanto lo va
-                                      // a leggere
+        // Vector parole = new Vector(); // vettore è un contenitore di oggetti
+        // (Object), qualsiasi. sta al programmatore
+        // stare attento a cosa ci mette dentro e a sapere cosa tira fuori quanto lo va
+        // a leggere
+        // List<String> parole2 = new ArrayList<String>(); //per retrocompatibilità
+        // bisogna dire String anche nel costruttore
+        List<String> parole = new ArrayList<>(); // poi l'hanno tolto dato che era un po' scocciante. lo capisce da
+                                                 // solo
+
         try (Scanner sc = new Scanner(System.in)) {
             while (sc.hasNext()) {
                 parole.add(sc.next());
@@ -17,18 +25,20 @@ public class BandieraVettore_DVD {
         }
 
         int maxLen = 0;
-        for (Object oggetto : parole) {
-            String parola = (String) oggetto; // il programmatore consapevole versa in una scatola [vector] della roba e
-                                              // deve essere consapevole di tirare fuori delle stringhe
+        for (String parola : parole) {
+            // String parola = (String) oggetto; // il programmatore consapevole versa in
+            // una scatola [vector] della roba e
+            // deve essere consapevole di tirare fuori delle stringhe
             int len = parola.length();
             if (len > maxLen)
                 maxLen = len;
             System.out.println(maxLen);
         }
 
-        for (Object oggetto : parole) {
-            String parola = (String) oggetto; // il programmatore consapevole versa in una scatola [vector] della roba e
-                                              // deve essere consapevole di tirare fuori delle stringhe
+        for (String parola : parole) {
+            // String parola = (String) oggetto; // il programmatore consapevole versa in
+            // una scatola [vector] della roba e
+            // deve essere consapevole di tirare fuori delle stringhe
             String parolaBandieraSpazi = String.format("%" + maxLen + "s", parola);
             System.out.println(parolaBandieraSpazi.replace(" ", "#"));
         }
